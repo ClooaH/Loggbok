055b570f04a7c17170ba3eff176aebb4d63b9273
ClooaH <a.s.d.w.a.s@hotmail.com>
2019-09-24 08:50:59 +0200

    Option 1,2 and 6 are now functional and available to be used properly.

--- src/Loggbokan.java
+++ src/Loggbokan.java
@@ -1,17 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Loggbokan {
     public static void main(String[] args) {
 
-        printMenu();
         Scanner in = new Scanner(System.in);
-        int choice = in.nextInt();
-        if (choice == 6) {
+        int securance = 1;
+        int countarray = 1;
+        ArrayList array = new ArrayList<LogEntry>();
+        while (securance == 1) {
+            printMenu();
+            int choice = in.nextInt();
+            in.nextLine();
+            if (choice == 1) {
 
-            System.exit(0);
+                System.out.println(array);
 
+            } else if (choice == 2) {
+
+                String p = in.nextLine();
+                array.add(0, new LogEntry(p));
+
+            } /* else if (choice == 3) {
+
+
+
+            } else if (choice == 4) {
+
+
+
+            } else if (choice == 5) {
+
+
+
+            } */ else if (choice == 6) {
+
+                securance = securance - 1;
+
+            }
         }
-
     }
 
     private static void printMenu() {
@@ -20,6 +49,6 @@ public class Loggbokan {
         "3. Redigera en gammal post i loggboken" + "\n" +
         "4. Spara loggboken" + "\n" +
         "5. Läs in en loggbok" + "\n" +
-        "6. Avsluta");
+        "6. Avsluta" + "\n");
     }
 }
